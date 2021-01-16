1c203e04cfc094169115766f95af30272102aff4
Stef Cruz <crzstefanie@gmail.com>
2021-01-16 11:02:35 +0000

    Add login functionality

--- app/login.py
+++ app/login.py
@@ -37,12 +37,34 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration successful")
+    # GET method, if user is already registered
     return render_template('register.html')
 
 
 # Log in
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # check if username exists in db
+        existing_user = DB_USERS.find_one(
+            {"username": request.form.get("username").lower()})
+        # if user in DB
+        if existing_user:
+            # check if password supplied matches db
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                # if matches, log in
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password
+                flash("Incorrect username and/or password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect username and/or password")
+            return redirect(url_for("login"))
+
     return render_template('login.html')
 
 # Log out
