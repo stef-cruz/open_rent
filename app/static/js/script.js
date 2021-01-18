09ccce47ce784282cdeda5517a74b1b39fd831ed
Stef Cruz <crzstefanie@gmail.com>
2021-01-18 22:22:30 +0000

    Add content to section 2 home page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,5 @@
+// MATERIALIZE INITS
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems,{edge: "right"});
