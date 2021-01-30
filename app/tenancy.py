6375bcb7f319ffaeca438d880c5dd5ac93e7334e
Stef Cruz <crzstefanie@gmail.com>
2021-01-30 16:44:52 +0000

    Enable user to update tenancy into the database

--- app/tenancy.py
+++ app/tenancy.py
@@ -80,9 +80,53 @@ def view_tenancy(tenancy_id):
 # EDIT TENANCY
 @app.route("/edit_tenancy/<tenancy_id>", methods=["GET", "POST"])
 def edit_tenancy(tenancy_id):
-    tenancy = DB_TENANCIES.find_one(
-        {'_id': ObjectId(tenancy_id)})
-    accommodation_types = DB_ACCOMMODATION_TYPES.find()
-    return render_template("edit-tenancy.html", tenancy=tenancy, accommodation_types=accommodation_types)
+    """Edit tenancy in database. Checks:
+        1- If user is logged in
+        2- If address is valid
+        3- If address is in Dublin
+
+        :return all tenancies
+        """
+
+    if session["user"]:
+        if request.method == 'POST':
+            # Geocode to get latitude and longitude of address,
+            # source https://github.com/googlemaps/google-maps-services-python
+            address = request.form.get('address_1')
+            geocode_result = gmaps.geocode(address)
+            # Extract lat and long from geocode_result,
+            # source https://stackoverflow.com/questions/37311687/extracting-lat-lon-from-geocode-result-list-with-python-google-maps-api
+            if geocode_result:
+                latitude = geocode_result[0]["geometry"]["location"]["lat"]
+                longitude = geocode_result[0]["geometry"]["location"]["lng"]
+                if 53.25 >= latitude >= 53.35 or -6.36 <= longitude <= -6.26:
+                    edited_tenancy = {
+                        "address_1": request.form.get("address_1"),
+                        "address_2": request.form.get("address_2"),
+                        "latitude": latitude,
+                        "longitude": longitude,
+                        "accommodation_type": request.form.get("accommodation_type"),
+                        "start_date": request.form.get("start_date"),
+                        "end_date": request.form.get("end_date"),
+                        "price": request.form.get("price"),
+                        "created_by": session["user"]
+                    }
+                    DB_TENANCIES.update({"_id": ObjectId(tenancy_id)}, edited_tenancy)
+                    flash("Tenancy Successfully Updated")
+
+                flash('Please enter an address in Dublin City.')
+                return redirect(url_for("edit_tenancy", tenancy_id=tenancy_id))
+
+            flash('Please enter a valid address.')
+            return redirect(url_for("edit_tenancy", tenancy_id=tenancy_id))
+
+        # Search for tenancy in DB
+        tenancy = DB_TENANCIES.find_one(
+            {'_id': ObjectId(tenancy_id)})
+        accommodation_types = DB_ACCOMMODATION_TYPES.find()
+        return render_template("edit-tenancy.html", tenancy=tenancy, accommodation_types=accommodation_types)
+
+    # If user not logged in
+    return redirect(url_for("login"))
 
 # DELETE TENANCY
