952fa14759438de7e551a6ef42b36e086a516ef7
Stef Cruz <crzstefanie@gmail.com>
2021-02-12 20:39:59 +0000

    Remove unused code from date picker function

--- app/static/js/startEndDateTenancy.js
+++ app/static/js/startEndDateTenancy.js
@@ -1,15 +1,10 @@
 //Code adapted from source https://codepen.io/luv2code/pen/oNvrWyZ
 
-// Create default dates
-var date = new Date();
-// set default date for #start_date (1 week from today)
-var nextWeekStartDate = new Date(date.setDate(date.getDate() + 7));
-// Default date for #end_date
-var nextWeekEndDate = new Date(date.setDate(nextWeekStartDate.getDate() + 7));
+// Create default date
+let date = new Date();
 
 // SET OPTIONS FOR #start_date
-var optionsStartDate = {
-    defaultDate: new Date(nextWeekStartDate),
+let optionsStartDate = {
     autoClose: true,
     showClearBtn: true,
     format: 'dd mmmm yyyy',
@@ -28,8 +23,7 @@ var optionsStartDate = {
 
 
 // SET OPTIONS FOR #end_date
-var optionsEndDate = {
-    defaultDate: new Date(nextWeekEndDate),
+let optionsEndDate = {
     autoClose: true,
     showClearBtn: true,
     format: 'dd mmmm yyyy',
@@ -49,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // FUNCTION TO SET MINIMUM DATE WHEN #start_date DATE SELECTED
 var setMinEndDate = function(MinEndDate) {
     // Get the current date set on #end_date datepicker
-    var selectedEndDate = M.Datepicker.getInstance(document.querySelector('#end_date'));
+    let selectedEndDate = M.Datepicker.getInstance(document.querySelector('#end_date'));
     selectedEndDate.options.minDate = MinEndDate;
 
     // Check if the #start_date date is greater than the #end_date date and modify by 1 day if true.
