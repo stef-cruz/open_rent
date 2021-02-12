74669308ae8d159cb699691575f2bb50c505ccfa
Stef Cruz <crzstefanie@gmail.com>
2021-02-12 21:59:01 +0000

    Refactor nav links

--- app/static/js/materialize.js
+++ app/static/js/materialize.js
@@ -4,9 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
     //Home page, side nav
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems,{edge: "right"});
+
+    var elemsDropdown = document.querySelectorAll('.dropdown-trigger');
+    var instanceDropdown = M.Dropdown.init(elemsDropdown, {
+        hover: true
+    });
+
     //Profile page, select dropdown
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
+
     //Profile page, Collapsible
     var elems = document.querySelectorAll('.collapsible.expandable');
     var instances = M.Collapsible.init(elems, {
