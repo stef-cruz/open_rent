93e9f5ffd25677a68ce671672c8ef9e3aa91ec27
Stef Cruz <crzstefanie@gmail.com>
2021-02-16 19:49:58 +0000

    Add event listener to button on contact page

--- app/static/js/email.js
+++ app/static/js/email.js
@@ -3,10 +3,16 @@
     emailjs.init("user_Oay0ciCVLZ3t84nUMqX94");
 })();
 
-//Send emailJS
-emailjs.sendForm('gmail', 'milestone3', '#contact-form')
-    .then(function (response) {
-        console.log('SUCCESS!', response.status, response.text);
-    }, function (error) {
-        console.log('FAILED...', error);
-    });
+let forms = document.getElementById('contact-form');
+
+forms.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    emailjs.sendForm('gmail', 'milestone2', '#contact-form')
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            document.getElementById("js-positive-feedback").style.display = "block";
+        }, function (error) {
+            console.log('FAILED...', error);
+            document.getElementById("js-negative-feedback").style.display = "block";
+        });
+})
